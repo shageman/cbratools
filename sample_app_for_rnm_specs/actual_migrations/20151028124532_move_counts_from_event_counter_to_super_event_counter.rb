1333791e1f902bbdaaa1636c83993cd3a55ae78f
Stephan Hagemann <stephan.hagemann@gmail.com>
2015-10-28 13:06:50 -0600

    Remove test output from git

--- sample_app_for_rnm_specs/actual_migrations/20151028124532_move_counts_from_event_counter_to_super_event_counter.rb
+++
@@ -1,5 +0,0 @@
-class MoveCountsFromEventCounterToSuperEventCounter < ActiveRecord::Migration
-  def change
-    rename_table :event_counter_counts, :super_event_counter_counts
-  end
-end
--- sample_app_for_rnm_specs/actual_migrations/20151028124532_move_things_from_event_counter_to_super_event_counter.rb
+++
@@ -1,5 +0,0 @@
-class MoveThingsFromEventCounterToSuperEventCounter < ActiveRecord::Migration
-  def change
-    rename_table :event_counter_things, :super_event_counter_things
-  end
-end
